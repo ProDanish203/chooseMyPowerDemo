c0339b8c85aa3c8ca3a569e33cdb0ce0b0b3d5fe
Danish <danishsidd524@gmail.com>
2023-09-29 23:12:07 +0500

    Initial commit

---
+++ components/shared/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <>
+    <footer className='text-center bg-primary py-3 text-white'>
+        <p>Made with <span className='text-lg text-red-500'>❤</span> by <span className='text-lg text-red-500'>Danish Siddiqui</span></p>
+    </footer>
+    </>
+  )
+}
---
+++ util/data.ts
@@ -0,0 +1,18 @@
+export const navLinks = [
+    {
+        name: "Home",
+        path: "/home",
+    },
+    {
+        name: "Providers",
+        path: "/providers",
+    },
+    {
+        name: "About Us",
+        path: "/about-us",
+    },
+    {
+        name: "Contact",
+        path: "/contact",
+    },
+]
